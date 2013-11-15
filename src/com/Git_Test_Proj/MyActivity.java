efc66ff0691af0b88c02374bd2c594f6fb724e48
Alexey <Rico42949>
2013-11-15 23:54:33 +0200

    added colors

--- src/com/Git_Test_Proj/MyActivity.java
+++ src/com/Git_Test_Proj/MyActivity.java
@@ -1,8 +1,11 @@
 package com.Git_Test_Proj;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class MyActivity extends Activity {
     /**
@@ -13,5 +16,16 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         Toast.makeText(this, "Fuck off", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onResume() {
+        Random random = new Random();
+        int color1 = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+        this.getWindow().setTitleColor(color1);
+        int color2 = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+        this.getWindow().getDecorView().setBackgroundColor(color2);
+        super.onResume();
     }
 }
